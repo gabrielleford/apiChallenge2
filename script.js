cb38f33255db0c187808ab318c5e60c2c490c968
Gabrielle Ford <gabrielledeva@gmail.com>
2021-09-27 19:43:37 -0400

    restricting date search

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
-let baseURL = 'https://api.nasa.gov/planetary/apod?api_key=xLBmSdVETcqUsOAY73zIXHUCrkUv1yxvFaRlLRjU&date=';
+let baseURL = 'https://api.nasa.gov/planetary/apod?api_key=xLBmSdVETcqUsOAY73zIXHUCrkUv1yxvFaRlLRjU&date=2019-10-04';
 let fullURL;
+
+let dateInput = document.querySelector('#date');
+dateInput.max = new Date().toISOString().split('T')[0];
 
 fetch(baseURL)
     .then(response => response.json())
