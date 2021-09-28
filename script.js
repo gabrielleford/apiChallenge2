1d2eb37f289142d620cc21adf1e3baa76547ebef
Gabrielle Ford <gabrielledeva@gmail.com>
2021-09-27 20:52:27 -0400

    Creating fetch function on form submit

--- script.js
+++ script.js
@@ -1,10 +1,26 @@
-let baseURL = 'https://api.nasa.gov/planetary/apod?api_key=xLBmSdVETcqUsOAY73zIXHUCrkUv1yxvFaRlLRjU&date=2019-10-04';
+// Creating URL variables for fetch
+let baseURL = 'https://api.nasa.gov/planetary/apod?api_key=xLBmSdVETcqUsOAY73zIXHUCrkUv1yxvFaRlLRjU&date=';
 let fullURL;
 
+// Grabbing form for event listener and creating a date variable
+let search = document.querySelector('form');
+let date;
+search.addEventListener('submit', fetchPicture);
+
+// Setting the max date allowed to current day
 let dateInput = document.querySelector('#date');
 dateInput.max = new Date().toISOString().split('T')[0];
 
-fetch(baseURL)
-    .then(response => response.json())
-    .then(json => console.log(json))
+// Function that will fetch the picture or video from APOD
+async function fetchPicture(e) {
+    e.preventDefault();
+
+    date = document.getElementById('date').value; // Assigning that inputted date to the date variable
+    console.log(date);
+    fullURL = `${baseURL}${date}`;
+
+    const response = await fetch(fullURL);
+    const json = await response.json();
+    console.log(json);
+}
 
